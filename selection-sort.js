288ce53653675894a57cfbeef62c6e257f2ae362
Kalen Moss <105221006@edu.cbe.ab.ca>
2022-09-12 12:01:52 -0600

    Selection adn linerar

--- selection-sort.js
+++ selection-sort.js
@@ -1,13 +1,21 @@
 function selectionSort(array) {
-	/*
-	 ** Search through the whole array for the smallest element
-	 ** then swap it with the first element.
-	 ** Next, search through the whole array (except the first element)
-	 ** for the smallest, then swap it with the second element.
-	 ** Continue like this until you have only one element left.
-	 */
-
-	return array;
-}
-
+		let arrayLength = array.length;
+			
+		for(let i = 0; i < arrayLength; i++) {
+			// Finding the smallest number in the subarray
+			let min = i;
+			for(let j = i+1; j < arrayLength; j++){
+				if(array[j] < array[min]) {
+					min=j; 
+				}
+			 }
+			 if (min != i) {
+				 // Swapping the elements
+				 let tmp = array[i]; 
+				 array[i] = array[min];
+				 array[min] = tmp;      
+			}
+		}
+		return array;		
+	}
 module.exports = selectionSort;
